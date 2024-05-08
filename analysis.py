ccbb7afbcc7c90cd3ea52ae44c3ea9caf62138e5
theresa smyth <teesesmit@hotmail.com>
2024-05-08 22:25:13 +0100

    import Iris data set in csv and import programs

--- analysis.py
+++ analysis.py
@@ -1,2 +1,13 @@
-# Starting project
-# checking if this works with git
+# This program researches the Iris data set
+# 1. Outputs a summary of each varaible to a single text file
+# 2. Saves a histogram of each variable to png files
+# 3. Outputs a scatter plot of each pair of variables.
+# 4. Further analysis
+# Author: Theresa Smyth
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Loads the Iris dataset from a CSV file
+iris_data = pd.read_csv('/Users/Theresa/Desktop/Pands Project/iris.csv')
